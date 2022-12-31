212051070109372a7adad03dd1a39bc3cfbf6166
Vinicius Corbelli <v.corbelli71@gmail.com>
2022-12-30 23:27:37 -0300

    feat; escrita em documento e menu

--- src/main/java/br/com/trabalho/dcc062/editor.java
+++ src/main/java/br/com/trabalho/dcc062/editor.java
@@ -1,63 +1,110 @@
 package br.com.trabalho.dcc062;
 
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class editor {
 
+	static List<String[]> editor = new ArrayList<String[]>();
+	static int qtdLinhas = 0;
+	static Scanner scanner = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		String[][] editor = new String[20][100];
 		boolean loop = true;
-		String diretorio = "";
 
-		try (Scanner scanner = new Scanner(System.in)) {
-			do {
-				System.out.println("Deseja editar o arquivo? (S/N) ");
-				String resposta = scanner.nextLine();
-				if (resposta.equalsIgnoreCase("s")) {
+		do {
+			try {
+				System.out.println(" 1 - Editar o documento. ");
+				System.out.println(" 2 - Ver o documento. ");
+				System.out.println(" 3 - Salvar. ");
+				System.out.println(" 0 - Sair. ");
+				int resposta = scanner.nextInt();
 
-					System.out.println("Digite a linha que deseja editar (Max: 100): ");
-					int linha = scanner.nextInt();
-					if (linha > 100 || linha < 1) {
-						System.out.println("O tamanho máximo do arquivo é de 100 linhas!");
-						continue;
-					}
+				switch (resposta) {
+				case 1:
+					editarDocumento();
+					break;
 
-					System.out.println("Digite a coluna que deseja editar (Max: 30): ");
-					int coluna = scanner.nextInt();
-					if (coluna > 30 || coluna < 1) {
-						System.out.println("O tamanho máximo do arquivo é de 30 colunas!");
-						continue;
-					}
-					scanner.nextLine();
+				case 2:
+					verDocumento();
+					break;
 
-					System.out.println("Digite o texto que deseja inserir em (" + linha + ", " + coluna + ") ");
-					System.out.println("Texto atual: " + editor[coluna - 1][linha - 1]);
-					String texto = scanner.nextLine();
-					editor[coluna - 1][linha - 1] = texto;
+				case 3:
+					salvarDocumento();
+					break;
 
-				} else if (resposta.equalsIgnoreCase("n")) {
+				case 0:
 					loop = false;
-				} else {
-					System.out.println("Resposta incorreta.");
+					break;
 				}
+			} catch (Exception e) {
+				System.out.println("Ocorreu um erro interno: " + e);
+				loop = false;
+			}
+		} while (loop == true);
+		System.out.println("Finalizado com sucesso!");
+	}
 
-			} while (loop == true);
-			
-			System.out.println("Qual o diretorio o arquivo será salvo?");
-			diretorio = scanner.nextLine();
-			
-		} catch (Exception e) {
-			System.out.println("Ocorreu um erro interno: " + e.getMessage());
+	private static void editarDocumento() throws Exception {
+		System.out.println("Digite a linha que deseja editar: ");
+		int linha = scanner.nextInt();
+
+		System.out.println("Digite a coluna que deseja editar: ");
+		int coluna = scanner.nextInt();
+
+		System.out.println("Digite o texto que deseja inserir em [" + linha + ", " + coluna + "] ");
+		if (qtdLinhas >= linha) {
+			System.out.println("Texto atual: " + editor.get(linha - 1)[coluna - 1]);
 		}
+		scanner.nextLine();
+		String texto = scanner.nextLine();
 
-		// Usar chamada de sistema para criar um arquivo e salvar o texto que o usuário criou.
+		verificaLinhas(linha);
+		editor.get(linha - 1)[coluna - 1] = texto;
+	}
+
+	private static void verificaLinhas(int linha) {
+		if (qtdLinhas < linha) {
+			for (int i = qtdLinhas; i < linha; i++) {
+				editor.add(new String[20]);
+				qtdLinhas = linha;
+			}
+		}
+	}
+
+	private static void verDocumento() throws Exception {
 		for (String[] linhas : editor) {
 			for (String texto : linhas) {
-				System.out.print(texto + " ");
+				if (texto != null) {
+					System.out.print(texto + " ");
+				}
 			}
 			System.out.println();
 		}
-		System.out.println("Arquivo salvo em: " + diretorio);
+	}
+
+	private static void salvarDocumento() throws Exception {
+		System.out.println("Qual arquivo será salvo?");
+		scanner.nextLine();
+		String diretorio = scanner.nextLine();
+
+		FileWriter arq = new FileWriter(diretorio);
+		try (PrintWriter printArq = new PrintWriter(arq)) {
+			for (String[] linhas : editor) {
+				for (String texto : linhas) {
+					if (texto != null) {
+						printArq.print(texto + " ");
+					}
+				}
+				printArq.println();
+			}
+			System.out.println("Arquivo salvo em: " + diretorio);
+		} catch (Exception e) {
+			throw new Exception("Erro ao salvar o arquivo:" + e.getMessage());
+		}
 
 	}
 
